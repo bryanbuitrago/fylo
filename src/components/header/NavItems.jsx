ba961f7f844d6f6f75c431ccf4daa62bf9879176
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-27 18:25:58 -0400

    Add NavItems component.

---
+++ src/components/header/NavItems.jsx
@@ -0,0 +1,14 @@
+const NavItems = () => {
+  return (
+    <>
+      <a href='#features' className='hover:text-accentCyan'>
+        Features
+      </a>
+      <a href='#testimonials' className='hover:text-accentCyan'>
+        Testimonials
+      </a>
+    </>
+  );
+};
+
+export default NavItems;
