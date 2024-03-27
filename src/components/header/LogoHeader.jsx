11e05bd3230c99bc4fd3b7d364f861232eff67ce
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-27 18:26:16 -0400

    Add LogoHeader component.

---
+++ src/components/header/LogoHeader.jsx
@@ -0,0 +1,11 @@
+const LogoHeader = () => {
+  return (
+    <div
+      className='bg-logo-light-mode dark:bg-logo-dark-mode 
+                 bg-no-repeat h-20 w-48 mx-auto md:mx-0 
+                 md:absolute top-10 left-10'
+    ></div>
+  );
+};
+
+export default LogoHeader;
