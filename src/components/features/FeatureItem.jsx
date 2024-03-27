f04ecbbe4d45a9822bf02e1a684269124ffa12aa
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-27 19:20:10 -0400

    Update max width of text.

--- src/components/features/FeatureItem.jsx
+++ src/components/features/FeatureItem.jsx
@@ -2,12 +2,12 @@ import Image from 'next/image';
 
 const FeatureItem = ({ title, text, imgSrc }) => {
   return (
-    <div className='flex flex-col items-center space-y-2 md:w-1/2'>
+    <div className='flex flex-col items-center space-y-2'>
       <div className='flex items-center justify-center h-24 mb-6'>
         <Image src={imgSrc} width={50} height={50} />
       </div>
       <h3 className='text-xl font-bold'>{title}</h3>
-      <p className='max-w-sm'>{text}</p>
+      <p className='max-w-md'>{text}</p>
     </div>
   );
 };
