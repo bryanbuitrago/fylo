38ed0bdbdf32b8169235922c79b65417b23265d9
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-27 18:41:19 -0400

    Add Hero component to Home page.

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Header from '@/components/header/Header';
+import Hero from '@/components/hero/Hero';
 
 export default function Home() {
   return (
     <main className=''>
       <Header />
+      <Hero />
     </main>
   );
 }
