8f578d1692f588d58fbee5d5790f55cd2774854e
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-27 18:18:41 -0400

    Add dark mode support and custom colors and backgrounds.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,30 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightRed: 'hsl(0, 100%, 63%)',
+        accentCyan: 'hsl(176, 68%, 64%)',
+        accentBlue: 'hsl(198, 60%, 50%)',
+        darkBlue: 'hsl(217, 28%, 15%)',
+        darkBlue1: 'hsl(218, 28%, 13%)',
+        darkBlue2: 'hsl(216, 53%, 9%)',
+        darkBlue3: 'hsl(219, 30%, 18%)',
+      },
+      backgroundImage: (theme) => ({
+        'curvy-dark-mode': "url('/images/bg-curvy-dark-mode.svg')",
+        'curvy-light-mode': "url('/images/bg-curvy-light-mode.svg')",
+        'logo-dark-mode': "url('/images/logo-dark-mode.svg')",
+        'logo-light-mode': "url('/images/logo-light-mode.svg')",
+      }),
+    },
+  },
+  variants: {
+    extend: {
+      backgroundImage: ['dark'],
+    },
   },
   plugins: [],
 };
