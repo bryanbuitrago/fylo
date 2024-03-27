fb1caa8003fc79a496dc5691b514bdfd8c5e1455
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-27 19:20:59 -0400

    Add featuresData.js file with initial data.

---
+++ src/components/features/featuresData.js
@@ -0,0 +1,22 @@
+export const FEATURE_ITEMS_DATA = [
+  {
+    imgSrc: '/images/icon-access-anywhere.svg',
+    title: 'Access your file from anywhere',
+    text: 'The ability to use a smartphone, tablet, or computer to access your account means your files follow you everywhere.',
+  },
+  {
+    imgSrc: '/images/icon-security.svg',
+    title: 'Security you can trust',
+    text: '2-factor authentication and user-controlled encryption are just a couple of the security features we allow to help secure your files.',
+  },
+  {
+    imgSrc: '/images/icon-collaboration.svg',
+    title: 'Real-time collaboration',
+    text: 'Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required.',
+  },
+  {
+    imgSrc: '/images/icon-any-file.svg',
+    title: 'Store any type of file',
+    text: "Whether you're sharing holidays photos or work documents, Fylo has you covered allowing for all file types to be securely stored and shared.",
+  },
+];
