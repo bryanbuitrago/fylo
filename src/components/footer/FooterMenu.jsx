50bd41682ecf88a15d0e5e678a30380aed99e39a
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-28 00:03:51 -0400

    Add padding top.

--- src/components/footer/FooterMenu.jsx
+++ src/components/footer/FooterMenu.jsx
@@ -1,6 +1,9 @@
 const FooterMenu = () => {
   return (
-    <div className='flex flex-col space-y-10 text-xl md:text-base md:space-x-20 md:space-y-0 md:flex-row'>
+    <div
+      className='flex flex-col space-y-10 text-xl md:pt-10 
+                 md:text-base md:space-x-20 md:space-y-0 md:flex-row'
+    >
       <div className='flex flex-col space-y-3'>
         <a href='#'>About Us</a>
         <a href='#'>Jobs</a>
