7daf384f6eb2d282b52a5a1d5542e1564f0a8f35
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-27 22:50:11 -0400

    Add testimonials data

---
+++ src/components/testimonials/testimonialsData.js
@@ -0,0 +1,23 @@
+export const TESTIMONIALS_DATA = [
+  {
+    testimonialText:
+      'Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.',
+    img: '/images/profile-1.jpg',
+    customerName: 'Satish Patel',
+    companyTitle: 'Founder & CEO, Huddle',
+  },
+  {
+    testimonialText:
+      'Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.',
+    img: '/images/profile-2.jpg',
+    customerName: 'Bruce McKenzie',
+    companyTitle: 'Founder & CEO, Huddle',
+  },
+  {
+    testimonialText:
+      'Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.',
+    img: '/images/profile-3.jpg',
+    customerName: 'Iva Boyd',
+    companyTitle: 'Founder & CEO, Huddle',
+  },
+];
