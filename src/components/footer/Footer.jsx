3b3c8abefdee33c8a75a680001d47bc592b15004
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-28 00:02:56 -0400

    Add text-white class.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import SocialLinks from './SocialLinks';
 
 const Footer = () => {
   return (
-    <footer className='bg-darkBlue2'>
+    <footer className='bg-darkBlue2 text-white'>
       <div className='container mx-auto px-5 pb-10'>
         <div className='flex flex-col items-center justify-between space-y-24 md:flex-row md:space-y-0'>
           <ContactInfo />
